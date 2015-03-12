101def3742a7c9e334a11869a389b3d2bc8ee9cd
Tommy Gibbons <tommygibbons@Tommys-MacBook-Pro-2.local>
2015-03-11 18:20:03 -0700

    third commit

--- MySafari/WebViewController.m
+++ MySafari/WebViewController.m
@@ -8,11 +8,14 @@
 
 #import "WebViewController.h"
 
-@interface WebViewController () <UIWebViewDelegate,UITextFieldDelegate>
+@interface WebViewController () <UIWebViewDelegate,UITextFieldDelegate, UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *networkActivityIndicator;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
+
 
 @end
 
@@ -22,6 +25,8 @@
     [super viewDidLoad];
     self.webView.delegate = self;
    [self performLoadRequestWithString:@"http://www.google.com"];
+    self.webView.scrollView.delegate = self;
+
 }
 
 
@@ -30,13 +35,23 @@
     [self performLoadRequestWithString:textField.text];
     [textField resignFirstResponder];
     return YES;
+
 }
+
 
 -(void)performLoadRequestWithString:(NSString *)string
 {
+    if([self.urlTextField.text hasPrefix: @"http://"]){
     NSURL *url = [NSURL URLWithString:string];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest: request];
+    }else{
+        NSMutableString *newURL = [NSMutableString stringWithString:string];
+        [newURL insertString:@"http://" atIndex:0];
+        NSURL *String2 = [NSURL URLWithString:newURL];
+        NSURLRequest *request = [NSURLRequest requestWithURL:String2];
+        [self.webView loadRequest: request];
+    }
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
@@ -50,7 +65,10 @@
 }
 
 - (IBAction)onForwardButtonPressed:(id)sender {
+    if (self.webView.canGoForward)//checks to see if on current page
+    {
     [self.webView goForward];
+    }
     
 }
 
@@ -58,10 +76,27 @@
     [self.webView stopLoading];
 }
 
-- (IBAction)onBackButtonPressed:(id)sender {
+- (IBAction)onBackButtonPressed:(id)sender
+{
+    if(self.webView.canGoBack){ //checks to see if on first page
     [self.webView goBack];
+    }
 }
 
+- (IBAction)onReloadButtonPressed:(id)sender
+{
+    [self.webView reload];
+}
+- (IBAction)onPlusButtonTap:(id)sender {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Coming Soon" message:nil delegate:self cancelButtonTitle:@"cancel" otherButtonTitles:nil, nil];
+    [alertView show];
+}
 
+#pragma mark -UIScrollViewDelegate Protocols
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self.urlTextField setHidden:YES];
+}
 
 @end
